b0240d09174d14740c5e6339298713cace631773
rabeeh003 <rabimon003@gmail.com>
2023-08-23 17:27:15 +0530

    set  togle butten with worcking function set side bar uncomplited design set footer section

---
+++ main.js
@@ -0,0 +1,10 @@
+// togle function
+function navOn(){
+    var dis = document.getElementById('snav');
+    if(dis.style.display ===  "none"){
+        dis.style.display ="block";
+    }else
+    {
+        dis.style.display ="none";
+    }
+}
